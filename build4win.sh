90780e80a44e8a77b59252ff30c685de8bac0e58
Ant <ant@none>
2021-03-25 21:43:45 +0000

    Zip package and build scripts for win

--- build4win.sh
+++ build4win.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
 py setup4win.py py2exe
-mv dist/main.exe dist/
+mv dist/main.exe dist/spautomod.exe
 cp -r data/ dist/data
 
 if [ ! -d dist/data/backups ]; then
   mkdir dist/data/backups
 fi
-
 if [ ! -d dist/data/logs ]; then
   mkdir dist/data/logs
 fi
@@ -15,5 +14,9 @@ fi
 if [ -d dist/data/test ]; then
   rm -r dist/data/test
 fi
+if [ -d dist/data/test ]; then
+  rm -r dist/data/images
+fi
 
 mv dist spautomod4win
+echo 'Start-Process -FilePath "spautomod.exe" -ArgumentList "--loop" -Wait -WindowStyle Maximized' > spautomod4win/spautomod-loop
